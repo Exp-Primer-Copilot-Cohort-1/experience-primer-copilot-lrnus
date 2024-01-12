b8e6e678599ab11d175efa0fc60b6b70f08f97da
lrnus <63069571+lrnus@users.noreply.github.com>
2024-01-12 16:22:38 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,37 +1,55 @@
-// Create a web server
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
+// Create web server
+var express = require('express');
+var router = express.Router();
+var db = require('../db');
+var bodyParser = require('body-parser');
 
-// Setup the view engine
-app.set('view engine', 'ejs');
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-// Setup body-parser
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Setup the static assets directories
-app.use('/css', express.static('assets/css'));
-app.use('/img', express.static('assets/img'));
-
-// Setup the comments array
-const comments = [
-  { name: 'John', message: 'Hello there!' },
-  { name: 'Mary', message: 'How are you?' },
-];
-
-// Setup the root route
-app.get('/', (req, res) => {
-  res.render('index', { comments: comments });
+// Get comments
+router.get('/', function(req, res, next) {
+    var sql = "SELECT * FROM comments";
+    db.query(sql, function(err, rows, fields) {
+        if (err) throw err;
+        res.send(rows);
+    });
 });
 
-// Setup the post route
-app.post('/', (req, res) => {
-  comments.push({ name: req.body.name, message: req.body.message });
-  res.redirect('/');
+// Get comment by id
+router.get('/:id', function(req, res, next) {
+    var sql = "SELECT * FROM comments WHERE id = ?";
+    db.query(sql, [req.params.id], function(err, rows, fields) {
+        if (err) throw err;
+        res.send(rows);
+    });
 });
 
-// Setup the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+// Add comment
+router.post('/', urlencodedParser, function(req, res, next) {
+    var sql = "INSERT INTO comments (id, user_id, post_id, content, created_at, updated_at) VALUES (NULL, ?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)";
+    db.query(sql, [req.body.user_id, req.body.post_id, req.body.content], function(err, result) {
+        if (err) throw err;
+        res.send(result);
+    });
 });
+
+// Update comment
+router.put('/:id', urlencodedParser, function(req, res, next) {
+    var sql = "UPDATE comments SET content = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ?";
+    db.query(sql, [req.body.content, req.params.id], function(err, result) {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+
+// Delete comment
+router.delete('/:id', function(req, res, next) {
+    var sql = "DELETE FROM comments WHERE id = ?";
+    db.query(sql, [req.params.id], function(err, result) {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+
+module.exports = router;
